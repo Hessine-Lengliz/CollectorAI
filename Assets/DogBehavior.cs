5cac92b3cf4f9d3895dfeb9f62d4f0901f53388d
LenglizHessine <lengliz.hessine@gmail.com>
2025-01-08 10:52:59 +0100

    ImgsPush

--- Assets/DogBehavior.cs
+++ Assets/DogBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Rendering.UI;
+
 
 public class DogBehavior : MonoBehaviour
 {
